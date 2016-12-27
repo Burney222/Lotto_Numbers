a1e8501fbd268f484df6073d983ed2e10d422aba
Daniel Berninghoff <daniel.berninghoff@gmail.com>
2016-12-27 19:07:06 +0100

    Neues Feature hinzugefuegt um Anzahl der Reihen festzulegen und direkt auszugeben

--- Lottozahlen.py
+++ Lottozahlen.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 import numpy as np
+import sys
 
 def zahlen_einlesen(filename):
     zahlen = []
@@ -21,30 +22,48 @@ def zahlen_einlesen(filename):
 
     return zahlen, beliebtheiten
 
-#(Normale) Zahlen einlesen
-zahlen, beliebtheiten = zahlen_einlesen("Zahlen_Beliebtheiten.txt")
 
-#Gewichte erzeugen
-gewichte = 1 / (np.asarray(beliebtheiten)**2)
-wahrscheinlichkeiten = gewichte/np.sum(gewichte)
+def zahlen_ziehen(zahlen, zahlen_wkten, superzahlen, superzahlen_wkten):
+    #Zahlen ziehen
+    lotto_zahlen = np.sort(np.random.choice(zahlen, size=6, replace=False, p=zahlen_wkten))
 
-#Zahlen ziehen
-lotto_zahlen = np.sort(np.random.choice(zahlen, size=6, replace=False, p=wahrscheinlichkeiten))
+    #Superzahl ziehen, bis man eine hat, die nicht auch als "normale" Zahl gezogen wurde
+    superzahl = lotto_zahlen[0]
+    while superzahl in lotto_zahlen:
+        superzahl = np.random.choice(superzahlen, p=superzahlen_wkten)
 
-
-
-#Das Ganze nun noch fuer die Superzahl
-superzahlen, superbeliebtheiten = zahlen_einlesen("Superzahlen_Beliebtheiten.txt")
-
-#Gewichte erzeugen
-supergewichte = 1 / (np.asarray(superbeliebtheiten)**2)
-superwahrscheinlichkeiten = supergewichte/np.sum(supergewichte)
-
-#Superzahl ziehen, bis man eine hat, die nicht auch als "normale" Zahl gezogen wurde
-superzahl = lotto_zahlen[0]
-while superzahl in lotto_zahlen:
-    superzahl = np.random.choice(superzahlen, p=superwahrscheinlichkeiten)
+    return lotto_zahlen, superzahl
 
 
-print("Lottozahlen: {}".format(", ".join(map(str,lotto_zahlen))))
-print("Superzahl: {}".format(superzahl))
+if __name__ == "__main__":
+    #Wieviele Reihen sollen gespielt werden?
+    if len(sys.argv) == 2:
+        n_reihen = int(sys.argv[1])
+    elif len(sys.argv) == 1:
+        n_reihen = 1
+    else:
+        print("BENUTZUNG: python {} <Anzahl_Reihen>".format(sys.argv[0]))
+
+    #(Normale) Zahlen einlesen
+    zahlen, beliebtheiten = zahlen_einlesen("Zahlen_Beliebtheiten.txt")
+
+    #Gewichte erzeugen
+    gewichte = 1 / (np.asarray(beliebtheiten)**2)
+    wahrscheinlichkeiten = gewichte/np.sum(gewichte)
+
+    #Das Ganze nun noch fuer die Superzahl
+    superzahlen, superbeliebtheiten = zahlen_einlesen("Superzahlen_Beliebtheiten.txt")
+
+    #Gewichte erzeugen
+    supergewichte = 1 / (np.asarray(superbeliebtheiten)**2)
+    superwahrscheinlichkeiten = supergewichte/np.sum(supergewichte)
+
+    #Reihen ziehen und ausgeben
+    print("{0:>10} | {1:>23} | {2:>9}".format("Reihe", "Lottozahlen", "Superzahl"))
+    for i in range(n_reihen):
+        lotto_zahlen, superzahl = zahlen_ziehen(zahlen, wahrscheinlichkeiten, superzahlen,
+                                                superwahrscheinlichkeiten)
+        print("{0:>10} | {1:>23} | {2:>9}".format(i+1, ", ".join(map(str,lotto_zahlen)),superzahl))
+
+
+raw_input()  #Noetig um die Ausgabe in Windows geoeffnet zu lassen
